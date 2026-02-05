2a46493aca3dac8d0b754c376e893acbacb33dfe
daxeshchauhan016-tech <daxeshchauhan016@gmail.com>
2026-02-05 11:01:38 +0530

    Add files via upload

---
+++ 4-ERC20.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DaxeshToken is ERC20, Ownable {
+
+    constructor() ERC20("DaxeshToken", "DTK") Ownable(msg.sender) {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
+
+    // MINT (only owner)
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // BURN (anyone can burn their own tokens)
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
